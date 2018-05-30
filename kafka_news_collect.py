db8f986b498405da486e70b00e2c7fefb78f1361
Yas Eft <yasi.mailak@gmail.com>
2018-05-30 15:31:26 +0800

    Create kafka_news_collect.py

---
+++ kafka_news_collect.py
@@ -0,0 +1,58 @@
+"""
+Created on Wed May 30 14:02:24 2018
+
+@author: yasi
+"""
+from kafka import KafkaConsumer, KafkaProducer
+import datetime as dt
+from dateutil.tz import gettz
+import re
+import pytz
+import json
+
+
+def main():
+#    #os.environ -> representing the string environment. they already have been set (os.environ["INPUT_TOPIC"]="abcd")
+#    if 'INPUT_TOPIC' in os.environ:
+#        input_topic = os.environ.get('INPUT_TOPIC')#will return one string
+#    else:
+#        print 'Missing INPUT_TOPIC in environment variables.'
+#        sys.exit(0)
+#    if 'GROUP_ID' in os.environ:
+#        grp_id = os.environ.get('GROUP_ID')
+#    else:
+#        print 'Missing GROUP_ID in environment variables.'
+#        sys.exit(0)
+#    if 'BOOTSTRAP_SERVERS' in os.environ:
+#        bootstrp_servers = os.environ.get('BOOTSTRAP_SERVERS')
+#    else:
+#        print 'Missing BOOTSTRAP_SERVERS in environment variables.'
+#        sys.exit(0)
+#    if 'OUTPUT_TOPIC' in os.environ:
+#        output_topic = os.environ.get('OUTPUT_TOPIC')
+#    else:
+#        print 'Missing OUTPUT_TOPIC in environment variables.'
+#        sys.exit(0) 
+    consumer = KafkaConsumer('article',group_id='article',bootstrap_servers=bootstrp_servers,value_deserializer=lambda m: json.loads(m.decode('utf-8')),enable_auto_commit=False)
+    if consumer:
+        tz = pytz.timezone('Asia/Kuala_Lumpur')#py 2.7
+        for msg in consumer:
+            try:
+                now_utc = dt.datetime.now(tz=tz)# py 2.7
+                now = now_utc.astimezone(gettz('Asia/Kuala_Lumpur'))
+                current_time=now_utc.isoformat()
+                file_name="kafka_grabbed_news/"+current_time+".txt"
+                news_file=open(file_name,"w")
+                news_file.write(msg)
+                news_file.write("\n")
+                print("news"+current_time+" has been written ")
+                news_file.close()
+            except KeyError,e:
+                print("ERR! no key for",e, "in id=", msg.key, "for json=",msg.value)
+            except Exception, e:
+                print(e,"Unexpected ERR occured for id=", msg.key, "for json=",msg.value)
+            consumer.commit()
+            
+        
+if __name__ == "__main__":
+   main()
